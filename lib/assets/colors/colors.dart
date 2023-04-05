27e1a682868b880f7f6e5c16cb3144f39d18c6e0
amine-asfar <74060479+amine-asfar@users.noreply.github.com>
2023-04-05 11:30:06 +0200

    ajouter CustomColors

---
+++ lib/assets/colors/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class CustomColors {
+  static const Color primaryColor = Color(0xff5F67EA);
+  static const Color secondaryColor = Color(0XFFFC77A6);
+  static const Color thirdColor = Color(0XFFF7F7F7);
+  static const Color textColor = Color(0xff747685);
+
+}
